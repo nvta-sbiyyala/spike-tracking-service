1244d847f9597401947fbc17ffa823d96cef8fdd
Shishir Biyyala <Shishir.Biyyala@invitae.com>
2020-04-02 11:57:23 -0400

    Simpler TransactionManager bean

--- src/main/kotlin/sat/spike/tracking/SpikeTrackingApp.kt
+++ src/main/kotlin/sat/spike/tracking/SpikeTrackingApp.kt
@@ -1,16 +1,25 @@
 package sat.spike.tracking
 
+import org.jetbrains.exposed.spring.SpringTransactionManager
 import org.jetbrains.exposed.sql.Database
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.transaction.annotation.EnableTransactionManagement
 import sat.spike.tracking.db.DatabaseMigrator
 import javax.sql.DataSource
 
 @SpringBootApplication
+@EnableTransactionManagement
 class SpikingTrackingApp(dataSource: DataSource, migrator: DatabaseMigrator) {
     init {
         Database.connect(dataSource)
         migrator.migrate()
+    }
+
+    @Bean
+    fun transactionManager(dataSource: DataSource): SpringTransactionManager {
+        return SpringTransactionManager(dataSource)
     }
 }
 
