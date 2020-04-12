d4463c00e830c75b498364fa6ceb6e6ef9a8f3b6
Shishir Biyyala <Shishir.Biyyala@invitae.com>
2020-04-12 18:13:47 -0400

    Use immutable idioms / kotlin goodness

--- src/main/kotlin/sat/spike/tracking/utils/Extensions.kt
+++ src/main/kotlin/sat/spike/tracking/utils/Extensions.kt
@@ -7,12 +7,11 @@ import java.sql.ResultSet
 
 fun JsonNode.toString(objectMapper: ObjectMapper): String = objectMapper.writeValueAsString(this)
 
-fun <T : Any> String.execAndMap(transform: (ResultSet) -> T): List<T> {
-    val result = arrayListOf<T>()
-    TransactionManager.current().exec(this) { rs ->
-        while (rs.next()) {
-            result += transform(rs)
-        }
-    }
-    return result
-}
+fun <T : Any> String.execAndMap(transform: (ResultSet) -> T): List<T> =
+    TransactionManager.current().exec(this) {
+        sequence<T> {
+            while (it.next()) {
+                yield(transform(it))
+            }
+        }.toList()
+    }.orEmpty()
